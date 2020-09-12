6be627d92381ffce489e2a77356eb92dd9bc4c6f
Jason Theiler <turret.jason@outlook.com>
2020-09-12 14:45:47 +0200

    Remove unused and deprecated dependencies
    - Update build script
    - Reorganize package.json

--- src/build.js
+++ src/build.js
@@ -6,7 +6,7 @@ const globals = require("./globals.json")
 
 const themeNames = ["kelp-forest", "kelp-forest-italic", "kelp-forest-bold"]
 
-for (let themeName of themeNames) {
+for (const themeName of themeNames) {
   const locals = require(`./${themeName}.json`)
 
   const theme = mustache.render(JSON.stringify(template), {
